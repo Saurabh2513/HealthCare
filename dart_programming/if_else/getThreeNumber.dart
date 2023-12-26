fc500ac087ba65396eaf2415c7b9fe844d64b065
Sagar Yadav <sagaryadav12369@gmail.com>
2023-12-26 12:57:41 +0530

    git push

---
+++ dart_programming/if_else/getThreeNumber.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+void main() {
+  print("Enter First Number:");
+  var a = int.parse(stdin.readLineSync()!);
+  print("Enter second Number:");
+  var b = int.parse(stdin.readLineSync()!);
+  print("Enter Third Number:");
+  var c = int.parse(stdin.readLineSync()!);
+
+  if (a >= b && a >= c) {
+    print("$a is greater number");
+  }
+   else if ( b >= a && b >= c) {
+    print("$b is greater number");
+  }
+  else{
+    print("$c greater number");
+  }
+}
