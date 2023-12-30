b0660d62931022542e1355d30b67202aaed8539e
Sagar Yadav <sagaryadav12369@gmail.com>
2023-12-30 16:59:44 +0530

    push

--- dart_programming/String/name_user.dart
+++ dart_programming/String/name_user.dart
@@ -1,13 +1,13 @@
 import 'dart:io';
 
 void main() {
-  stdout.write('Enter your first name: ');
+  print('Enter your first name: ');
   String firstName = stdin.readLineSync()!;
 
-  stdout.write('Enter your middle name: ');
+  print('Enter your middle name: ');
   String middleName = stdin.readLineSync()!;
 
-  stdout.write('Enter your last name: ');
+  print ('Enter your last name: ');
   String lastName = stdin.readLineSync()!;
 
   String fullName = '$firstName $middleName $lastName';
